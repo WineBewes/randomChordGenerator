b64665749294265d2212c25116516a8210c9c38f
WineBewes <wine.bewes@gmail.com>
2018-12-06 16:12:07 +0100

    clean

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import RandomChordGenerator from './randomChordGenerator';
-
-function AppViewModel() {
-	const randomChordGenerator = new RandomChordGenerator();
-	randomChordGenerator.generate(10);
-}
-
-// Activates knockout.js
-ko.applyBindings(new AppViewModel());
