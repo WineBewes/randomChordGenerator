3481c89260a9df02c2f5388282e0a2dd8fe902d8
WineBewes <wine.bewes@gmail.com>
2018-12-07 23:46:55 +0100

    klaar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { RandomBanjoChordGenerator, Chord } from './randomChordGenerator/randomBanjoChordGenerator'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [RandomBanjoChordGenerator]
 })
+
 export class AppComponent {
-  title = 'app';
+  constructor(private randomBanjoChordGenerator: RandomBanjoChordGenerator) {
+    this.chords = new Array<Chord>();
+  }
+
+  title = 'Random Banjo Chord Generator';
+  get withCharacter(): boolean {
+    return this.withMinor || this.withDominant7 || this.withDiminished;
+  }
+  withAccidentals = true;
+  withMinor = true;
+  withDominant7 = true;
+  withDiminished = true;
+  numberOfChordsList = [3, 5, 10];
+  numberOfChords = 5;
+
+  chords: Array<Chord>;
+
+  getRandomChords() {
+    this.chords = this.randomBanjoChordGenerator.generate(this.numberOfChords, this.withAccidentals,
+                  this.withMinor, this.withDominant7, this.withDiminished);
+  }
 }
---
+++ src/app/randomChordGenerator/randomBanjoChordGenerator.ts
@@ -0,0 +1,125 @@
+
+export class RandomBanjoChordGenerator  {
+
+    private notes = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+    private accidentals = ['b', '#'];
+    private positions = ['R', '1', '2'];
+
+    public generate(numberOfChords: number, withAccidentals: boolean, withMinor: boolean,
+                    withDominant7: boolean, withDimished: boolean): Array<Chord> {
+
+        const chords = new Array<Chord>();
+
+        for (let i = 0; i < numberOfChords; i++) {
+
+            let chord = this.getChord(numberOfChords, withAccidentals, withMinor, withDominant7, withDimished);
+
+            // geen dubbele chords
+            while (chords.some((c: Chord) => c.note === chord.note && c.position === chord.position && c.character === chord.character)) {
+                chord = this.getChord(numberOfChords, withAccidentals, withMinor, withDominant7, withDimished);
+            }
+
+            chords.push(chord);
+
+        }
+
+        return chords;
+    }
+
+    private getChord(numberOfChords: number, withAccidentals: boolean, withMinor: boolean,
+        withDominant7: boolean, withDimished: boolean): Chord {
+
+        const note = this.getNoteWithEventualAccidental(withAccidentals);
+
+        const position = this.getPosition();
+
+        const character = this.getEventualCharacter(withMinor, withDominant7, withDimished);
+
+        return new Chord(note, position, character);
+    }
+
+    private getNoteWithEventualAccidental(withAccidentals: boolean): string {
+
+        let randomIndex = Math.floor(Math.random() * this.notes.length);
+
+        const note = this.notes[randomIndex];
+
+        if (!withAccidentals) {
+            return note;
+        }
+
+        // we geven natuurlijke noten een voordeel van 2 op 3.
+        const chance = Math.floor(Math.random() * 3);
+
+        if (chance <= 1) {
+            return note;
+        }
+
+        randomIndex = Math.floor(Math.random() * this.accidentals.length);
+
+        const accidental = this.accidentals[randomIndex];
+
+        if ((note === 'C' || note === 'F') && accidental === 'b') {
+            return note + '#';
+        }
+
+        if ((note === 'B' || note === 'E') && accidental === '#') {
+            return note + 'b';
+        }
+
+        return note + accidental;
+    }
+
+    private getPosition(): string {
+
+        const randomIndex = Math.floor(Math.random() * this.positions.length);
+
+        return this.positions[randomIndex];
+    }
+
+    private getEventualCharacter(withMinor: boolean, withDominant7: boolean, withDimished: boolean): string {
+
+        if (!(withMinor || withDominant7 || withDimished)) {
+            return '';
+        }
+
+        // we geven majeur een voordeel van 2 op 3.
+        const chance = Math.floor(Math.random() * 3);
+
+        if (chance <= 1) {
+            return '';
+        }
+
+        const characters = [];
+
+        if (withMinor) {
+            characters.push('m');
+        }
+
+        if (withDominant7) {
+            characters.push('7');
+        }
+
+        if (withDimished) {
+            characters.push('Ø');
+        }
+
+        const randomIndex = Math.floor(Math.random() * characters.length);
+
+        return characters[randomIndex];
+
+    }
+}
+
+export class Chord {
+    constructor(note: string, position: string, character: string) {
+        this.note = note;
+        this.position = position;
+        this.character = character;
+    }
+    public note: string;
+    public position: string;
+    public character: string;
+}
+
+
